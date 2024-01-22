7067a37bfa14a072ec361299b7088570974936d3
kevin_yang <kevin_yang@comwave.com.tw>
2024-01-22 18:10:14 +0800

    增加內容

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import React, { useReducer, useState } from "react";
 import "./App.css";
+import Todo from "./Todo";
+import { ACTIONS } from "./Actions";
 
 const reducer = (todos, action) => {
   console.log(todos, action)
+  const { todoContent, id } = action.payload
   switch (action.type) {
-    case "ADD":
-      return [...todos, { todoContent: action.payload.todoContent }]
+    case ACTIONS.ADD:
+      return [
+        ...todos,
+        newTodo(todoContent)
+      ];
+    case ACTIONS.TOGGLE:
+      return todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE:
+      return todos.filter((todo) => todo.id !== id);
+    default:
+      return todos;
+  }
+};
+
+const newTodo = (todoContent) => {
+  return {
+    id: Math.floor(Math.random() * 100000),
+    todoContent: todoContent,
+    complete: false
   }
 }
 
@@ -18,7 +43,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: "ADD", payload: { todoContent: todoContent } })
+    dispatch({ type: ACTIONS.ADD, payload: { todoContent: todoContent } })
   }
 
   return (
@@ -33,6 +58,9 @@ function App() {
           placeholder="Type in Somethig ..."
         />
       </form>
+
+      {/* 当你使用 {} 包裹代码块时，如果这个代码块包含 JSX，你应该使用小括号 () 来确保 JSX 元素被视为一个表达式。这样 JSX 元素就能够正确地在 React 中进行渲染 */}
+      {todos.map((todo) => (<Todo todo={todo} dispatch={dispatch} />))}
     </div>
   )
 }
